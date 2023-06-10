2520861f3a5dfef1a64c1a08db694b6d8bb7cc63
techietee <mailtomodupe@gmail.com>
2023-06-10 21:40:04 +0100

    updates scss for error page

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -1,16 +1,15 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page">
+    <div style={{margin:'200px 600px', textAlign:'center'}}>
       <h1>Oops!</h1>
-      <p>Sorry, Please go back home.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+      <p>you missed your way</p>
+      <p>Sorry, Please go back <Link to='https://boas.netlify.app/'>home.</Link></p>
+     
     </div>
   );
 }
