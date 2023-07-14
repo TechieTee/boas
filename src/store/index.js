085ee86fa7823cce86ba33590e0b1f28a6d74c8a
Fatima Modupe Bello <mailtomodupe@gmail.com>
2023-07-15 00:48:13 +0100

    created a boiler plate for the vuex store module

---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+//import the createStore object from Vuex
+import { createStore } from 'vuex'
+// Import axios to make HTTP requests
+import axios from "axios"
+export default createStore({
+    state: {},
+    getters: {},
+    actions: {},
+    mutations: {}
+})
